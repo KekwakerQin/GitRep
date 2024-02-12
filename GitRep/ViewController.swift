8915c9e263516ad00952b4df2170af6f22de48e6
Kekwaker <Kekwaker@gmail.com>
2024-02-12 12:54:48 +0800

    Initial Commit

---
+++ GitRep/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GitRep
+//
+//  Created by 秦钦吉思 on 12.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // dictionary.
+        var Peoples: [String: [String]] = [:]
+        
+        Peoples["Jennie"] = ["Cat", "Bob"]
+        Peoples["Cat"] = ["Dylan", "Karl"]
+        Peoples["Bob"] = ["Node", "Clare"]
+        
+        // Commit
+     }
+
+
+}
+
